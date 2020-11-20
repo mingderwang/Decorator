2e88d35b29675c1639ab7becfb5ef2b7d80085cb
Ming-der Wang <mingder78@yahoo.com>
2020-11-20 07:01:03 +0000

    add reame for decorator in Python

--- README.md
+++ README.md
@@ -3,3 +3,20 @@
 
 # UML Class Diagram
 ![](https://sourcemaking.com/files/v2/content/patterns/Decorator__1.png)
+
+# decorator macro in Python
+```
+from functools import wraps
+
+def debug(func):
+    @wraps(func)
+    def out(*args, **kwargs):
+        print(func.__name__)
+        return func(*args, **kwargs)
+    return out
+
+@debug
+def add(x, y):
+    return x + y
+
+```
--- main.py
+++ main.py
@@ -73,3 +73,19 @@ def main():
 
 if __name__ == "__main__":
     main()
+
+# in Python
+print('------------ using @wraps() ----')
+from functools import wraps
+def my_decorator(f):
+    @wraps(f)
+    def wrapper(*args, **kwds):
+        print('Calling decorated function')
+        return f(*args, **kwds)
+    return wrapper
+@my_decorator
+def example():
+    """Docstring"""
+    print('Called example function')
+
+example()
